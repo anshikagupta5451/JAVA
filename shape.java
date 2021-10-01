ef82bff4ebbfebf2da85c2150aeea3244bc70949
anshikagupta5451 <87441767+anshikagupta5451@users.noreply.github.com>
2021-10-01 21:36:08 +0530

    Create shape.java

---
+++ shape.java
@@ -0,0 +1,14 @@
+public class Shape {
+    static void draw() {
+        System.out.println("drawing");
+    }
+
+    static void erase() {
+        System.out.println("erasing");
+    }
+
+    public static void main(String[] args) {
+        draw();
+        erase();
+    }
+}
